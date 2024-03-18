e7e82e3201addf47421de2e100706fbdad485519
ASHMIRC <126319208+ASHMIRC@users.noreply.github.com>
2024-03-18 14:40:41 +0530

    Create Binary Search User Input Inorder.py

---
+++ Binary Search User Input Inorder.py
@@ -0,0 +1,43 @@
+class node:
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.data=data
+
+def create(root,n):
+    if root is None:
+        return node(n)
+    else:
+        temp=root
+        temp1=root
+        flag=0
+        while True:
+            if temp.left is None:
+                temp.left = node(n)
+                break
+            elif temp.right is None:
+                temp.right = node(n)
+                break
+            elif flag == 0:
+                temp = temp1.left
+                flag = 1
+            else:
+                temp = temp1.right
+                flag=0
+                temp1=temp1.left
+        return root
+
+def inorder(temp):
+    if temp:
+        
+        inorder(temp.left)
+        print(temp.data)
+        inorder(temp.right)
+root=None
+while True:
+    n=int(input("enter the val: "))
+    if n>0:
+        root=create(root,n)
+    else:
+        break
+inorder(root)
