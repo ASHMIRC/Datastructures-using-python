6eee8f9a3879b4c859e68805df25d50ed37742fa
ASHMIRC <126319208+ASHMIRC@users.noreply.github.com>
2024-03-18 14:47:00 +0530

    Create Binary Search Tree Inorder.py

---
+++ Binary Search Tree Inorder.py
@@ -0,0 +1,24 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+def bst(root,val):
+    if root is None:
+        return node(val)
+    elif root.data>val:
+        root.left=bst(root.left,val)
+    elif root.data<val:
+        root.right=bst(root.right,val)
+    return root
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+root=node(40)
+n=int(input("enter the val: "))
+for i in range(n):
+    ele=int(input("enter the ele: "))
+    bst(root,ele)
+inorder(root)
